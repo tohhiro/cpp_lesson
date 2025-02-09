8bda649c9951c4c1c94f92c52edfa223513bd4fe
hiroto <tohhiro@gmail.com>
2025-02-09 19:22:37 +0900

    関数とiostreamを混在させた出力の練習

--- src/sample.cpp
+++ src/sample.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "Hello world!";
+    cout << "Say Hello world";
     return 0;
 }
---
+++ src/sample_func.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    printf("関数を使った出力なのです\n");
+    cout << "ストリームを使った出力です\n";
+    return 0;
+}

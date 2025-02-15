3f2f5e48222e11ee7f57e727ac05e35c874c5d2d
hiroto <tohhiro@gmail.com>
2025-02-15 16:14:32 +0900

    少数から整数へキャストして表示する関数を追加

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int convertToInt();
+
 int main() {
-    cout << "Say Hello world";
+    convertToInt();
+    return 0;
+}
+
+int convertToInt(){
+    double pi = 3.14;
+    cout << (int)pi << endl;
     return 0;
 }

105637098835a8e68e5e082243a7d32887fae250
hiroto <tohhiro@gmail.com>
2025-03-01 16:32:31 +0900

    さらに動作する関数を追加

--- src/sample.cpp
+++ src/sample.cpp
@@ -30,7 +30,7 @@ int main() {
     // whileFunction();
     // doWhileFunction();
     // twiceMessage();
-    // arrayFunction();
+    arrayFunction();
     externalArgsArrayFunc();
     averageFunc();
     return 0;

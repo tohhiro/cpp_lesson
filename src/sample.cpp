c2ca844259e97fbb760e519056d27fa0a1f225fb
hiroto <tohhiro@gmail.com>
2025-03-01 16:31:02 +0900

    copilot PRレビューを試す

--- src/sample.cpp
+++ src/sample.cpp
@@ -31,7 +31,7 @@ int main() {
     // doWhileFunction();
     // twiceMessage();
     // arrayFunction();
-    // externalArgsArrayFunc();
+    externalArgsArrayFunc();
     averageFunc();
     return 0;
 }
